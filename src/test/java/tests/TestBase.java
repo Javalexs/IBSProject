52b99b5e067b2926e8223587703eae0eba439a68
Alexsey Fadeev <fadeevlesha@list.ru>
2023-03-01 00:14:17 +0300

    add remote

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,18 +14,15 @@ import helpers.Attach;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
-    public GeneralPage generalPage;
-    public RegPage regPage;
-    public LocatorPage locatorPage;
+    public GeneralPage generalPage = new GeneralPage();
+    public RegPage regPage = new RegPage();
+    public LocatorPage locatorPage = new LocatorPage();
     @BeforeEach
-    void addSite(){
+    static void addSite(){
         open("https://ibs.ru/career/jobs/inzhener-po-avtomatizatsii-testirovaniya-java-lyuboy-region-rf-udalennaya-rabota/");
-        Configuration.browserSize = "1024x768";
+        Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        generalPage = new GeneralPage();
-        regPage = new RegPage();
-        locatorPage = new LocatorPage();
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
