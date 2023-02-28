66251e74627a6ad9275fa99917386a8091e0a849
Alexsey Fadeev <fadeevlesha@list.ru>
2023-02-28 10:43:02 +0300

    add Attach

--- src/test/java/page/RegPage.java
+++ src/test/java/page/RegPage.java
@@ -38,8 +38,6 @@ public class RegPage {
     }
     public RegPage checkBox(){
         $(".checkbox label span").scrollIntoView(true).click();
-
-//        $(".form-footer").$(".checkbox label span").click();
         return this;
     }
 
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,10 +1,14 @@
 package tests;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.chrome.ChromeOptions;
 import page.GeneralPage;
 import page.LocatorPage;
 import page.RegPage;
+import helpers.Attach;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -18,7 +22,17 @@ public class TestBase {
 //        Configuration.browserSize = "1920x1080";
         Configuration.browserSize = "1024x768";
         Configuration.holdBrowserOpen = true;
-
+    }
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
 }
