c3da716999278cb670d2eef03ac54c0a09acecd5
Alexsey Fadeev <fadeevlesha@list.ru>
2023-03-01 13:08:27 +0300

    add beforeall

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import page.GeneralPage;
