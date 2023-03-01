f07c4f0237f226b34db461d8dbd2b31e4e0199e5
Alexsey Fadeev <fadeevlesha@list.ru>
2023-03-01 12:13:15 +0300

    add deleteimport

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -18,7 +18,7 @@ public class TestBase {
     public RegPage regPage = new RegPage();
     public LocatorPage locatorPage = new LocatorPage();
     @BeforeEach
-    static void addSite(){
+    void addSite(){
         open("https://ibs.ru/career/jobs/inzhener-po-avtomatizatsii-testirovaniya-java-lyuboy-region-rf-udalennaya-rabota/");
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
