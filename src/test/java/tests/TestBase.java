41babb5fcf1c95ecb3c9bf8fd271d4975815946f
Alexsey Fadeev <fadeevlesha@list.ru>
2023-03-01 13:13:21 +0300

    add baseUrl

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -20,7 +20,8 @@ public class TestBase {
     public LocatorPage locatorPage = new LocatorPage();
     @BeforeAll
     static void addSite(){
-        open("https://ibs.ru/career/jobs/inzhener-po-avtomatizatsii-testirovaniya-java-lyuboy-region-rf-udalennaya-rabota/");
+        Configuration.baseUrl = "https://ibs.ru/career/jobs/inzhener-po-avtomatizatsii-testirovaniya-java-lyuboy-region-rf-udalennaya-rabota/"
+//        open("https://ibs.ru/career/jobs/inzhener-po-avtomatizatsii-testirovaniya-java-lyuboy-region-rf-udalennaya-rabota/");
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
