550f99b4dd424cca8dd2d89f3f49f65744cc4073
Alexsey Fadeev <fadeevlesha@list.ru>
2023-03-02 17:13:36 +0300

    add othertest

--- src/test/java/tests/OtherTests.java
+++ src/test/java/tests/OtherTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-@Tag("start")
+
 @Tag("stop")
 public class OtherTests extends TestBase{
     @Test
