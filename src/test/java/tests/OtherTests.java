d8766a77e5a6a6c8b7a7fc5faceabe113afefdaf
Alexsey Fadeev <fadeevlesha@list.ru>
2023-03-02 17:30:42 +0300

    add disabled

--- src/test/java/tests/OtherTests.java
+++ src/test/java/tests/OtherTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 @Tag("stop")
 public class OtherTests extends TestBase{
     @Test
+    @Disabled
     @DisplayName("Проверка наличия заголовка вакансии")
     void checkBannerTest(){
         generalPage.bannerTest();
