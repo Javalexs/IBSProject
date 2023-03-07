4304e75f2048b8821d79fe2787cb9fb05b219213
Alexsey Fadeev <fadeevlesha@list.ru>
2023-03-07 11:26:36 +0300

    add useJUnitPlatform

--- src/test/java/page/FailPage.java
+++ src/test/java/page/FailPage.java
@@ -1,20 +1,19 @@
 package page;
 
-import static com.codeborne.selenide.Condition.hidden;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
 public class FailPage {
     public FailPage pathFailTest(String value){
-        step("Поиск слова \"Тестирование\" в навигации", () -> {
+        step("Поиск слова \"Вакансии\" в навигации", () -> { //для того чтобы тест упал ввести слово "Тестирование"
             $(".breadcrumbs").shouldHave(text(value));
         });
         return this;
     }
     public FailPage faviconFailTest(){
         step("Проверка на отсутствие фавикона", () -> {
-            $(".wrap a").shouldBe(hidden);
+            $(".wrap a").shouldBe(visible);//для того чтобы тест упал ввести "hidden"
         });
         return this;
     }
